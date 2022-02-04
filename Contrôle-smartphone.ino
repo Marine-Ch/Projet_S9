52ebad0326eac84c0841712fd27abac07bccbc75
Marine-Ch <66271056+Marine-Ch@users.noreply.github.com>
2022-02-04 08:50:01 +0100

    Add files via upload

---
+++ Contrôle-smartphone.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>
+
+//Pin laser si utilisation ON/OFF
+const int pin_led = 13; 
+
+//Pin potentiometre et laser si utilisation PWM
+//const int pin_pot = A0;
+//const int pin_led = 10;
+//int pot = 0;
+String message;
+
+
+void setup() 
+{
+	Serial.begin(9600);
+
+	// Utilisation ON/OFF
+	digitalWrite(pin_led, LOW);
+	
+
+	// Utilisation PWM
+	//AnalogWrite(pin_led,0);
+	
+
+	pinMode(pin_led, OUTPUT);
+}
+
+
+void loop() 
+{
+	//pot = analogRead(pin_pot);  //A activer si PWM
+
+	if (Serial.available() > 0 )   
+	{
+	   message=Serial.readString();     
+		if(message=="1")
+	   		{
+			//  ON/OFF
+   			digitalWrite(pin_led, HIGH); 
+   
+			// PWM
+			//analogWrite(pin_led,pot);
+   			}
+    	
+		if(message=="0")
+			{
+			//  ON/OFF
+         	digitalWrite(pin_led, LOW);  
+
+			// PWM
+			//analogWrite(pin_led,0);
+   			}
+	}
+}
+
+
+
+
+
